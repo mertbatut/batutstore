778fc6db514fab74951f1536f21fac0bb19a5fa7
mertbatut <mertbatut@gmail.com>
2024-05-03 14:54:14 +0300

    carousel tamamlandı

--- src/components/Carousels.jsx
+++ src/components/Carousels.jsx
@@ -3,15 +3,15 @@ import { Carousel, Typography, Button } from "@material-tailwind/react";
 
 export default function CarouselWithContent() {
   return (
-    <div >
-      <Carousel className="rounded-xl">
-        <div className="relative overflow-hidden">
+    <div className='flex justify-center' >
+      <Carousel className="w-[130rem] h-[40rem] overflow-hidden">
+        <div className="relative h-full w-full ">
           <img
             src="../home1.jpg"
             alt="image 1"
             className="h-full w-full object-cover"
           />
-          <div className="absolute inset-0 grid h-full w-full place-items-center  bg-black/50">
+          <div className="absolute inset-0 grid h-full w-full place-items-center  bg-black/30">
             <div className="w-3/4 text-center md:w-2/4">
               <Typography
                 variant="h1"
@@ -24,10 +24,10 @@ export default function CarouselWithContent() {
               <Typography
                 variant="lead"
                 color="white"
-                className="mb-12 opacity-80"
+                className="mb-12 opacity-80 "
               >
-                We know how large objects will act, but things on a
-                small scale just do not act that way.
+               <h4 className='font-normal text-xl'>We know how large objects will act, but things on a
+                small scale just do not act that way.</h4> 
               </Typography>
               <div className="flex justify-center gap-2">
                 <Button className='w-52 h-16 rounded-md px-4 py-4 gap-2.5 font-bold text-2xl' size="lg" color="blue">
@@ -44,7 +44,7 @@ export default function CarouselWithContent() {
             alt="image 1"
             className="h-full w-full object-cover"
           />
-          <div className="absolute inset-0 grid h-full w-full place-items-center bg-black/50">
+          <div className="absolute inset-0 grid h-full w-full place-items-center bg-black/30">
             <div className="w-3/4 text-center md:w-2/4">
               <Typography
                 variant="h1"
@@ -76,7 +76,7 @@ export default function CarouselWithContent() {
             alt="image 1"
             className="h-full w-full object-cover"
           />
-          <div className="absolute inset-0 grid h-full w-full place-items-center  bg-black/50">
+          <div className="absolute inset-0 grid h-full w-full place-items-center  bg-black/30">
             <div className="w-3/4 text-center md:w-2/4">
               <Typography
                 variant="h1"
