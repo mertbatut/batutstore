9c8e8ecde325c255edf4480e19bcdb89e3674fcb
mertbatut <mertbatut@gmail.com>
2024-05-06 11:58:57 +0300

    Footer Tamamlandı

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,9 +5,9 @@ export default class Footer extends Component {
     render() {
         return (
             <div className='FooterMain bg-[#252B42] '>
-                <div className='FooterFirst '>
-                    <div className='Entry flex justify-between items-center px-96 py-10'>
-                        <div className='FooterIn flex flex-col content-start'> 
+                <div className='FooterFirst  py-10'>
+                    <div className='Entry flex justify-around'>
+                        <div className='FooterIn flex flex-col content-start gap-2.5'> 
                             <h3 className='text-[#FFFFFF] font-bold text-2xl leading-8'>Consulting Agency For Your Business</h3>
                             <p className='text-[#FFFFFF] font-normal text-sm leading-5'>the quick fox jumps over the lazy dog</p>
                             </div>
@@ -22,7 +22,7 @@ export default class Footer extends Component {
                     </div>
 
                 </div>
-                <div className='FooterSecond flex justify-center gap-36'>
+                <div className='FooterSecond flex justify-center gap-40 py-12'>
                             <div className='FooterContainer'>
                                 <div className='FooterRow text-[#FFFFFF] list-none flex flex-col gap-5 '>
                                     <h5 className='font-bold text-base leading-6'>Company Info</h5>
@@ -89,7 +89,18 @@ export default class Footer extends Component {
                                 
                             </div>
                         </div>
-                <div className='FooterThird'></div>
+                <div className='FooterThird py-12 '>
+                    <div className='SocialFooter flex justify-around items-center gap-64'>
+                        <div className='SocialText text-[#FFFFFF]'>
+                            <h6 className='text-bold text-sm leading-6'>Made With Love By Finland All Right Reserved </h6>
+                        </div>
+                        <div className='SocialLogo flex gap-5'>
+                        <i class="fa-brands fa-facebook fa-xl text-[#23A6F0]"></i>
+                        <i class="fa-brands fa-instagram fa-xl text-[#23A6F0]"></i>
+                        <i class="fa-brands fa-twitter fa-xl text-[#23A6F0]"></i>
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
