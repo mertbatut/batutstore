db1ab3f7cbd29658248422a9afe462c6b1e2a833
mertbatut <mertbatut@gmail.com>
2024-05-06 13:06:51 +0300

    Product-Card-25 HTML şeması

---
+++ src/layout/PageContent.jsx
@@ -0,0 +1,150 @@
+import React, { Component } from 'react'
+
+export default class
+    extends Component {
+    render() {
+        return (
+            <div>
+                <div className='BestSeller'>
+                    <div className='FurnitureCard'>
+                        <div className='ItemFurniture'>
+                            <h6>FURNITURE</h6>
+                            <h6>5 Items</h6>
+                        </div>
+                        <div className='FurBackground'>
+                            <img src="#" alt="" />
+                        </div>
+                    </div>
+                    <div className='BestProduct'>
+                        <div className='BestMain'>
+                            <div className='BestRow1'>
+                                <div className='BestContent'>
+                                    <h3>BESTSELLER PRODUCTS</h3>
+                                </div>
+                                <div className='BestNav'>
+                                    <div className='BestNav1'>
+                                        <li>Sola ok</li>
+                                        <li>Sağa ok</li>
+                                    </div>
+                                    <div className='BestNav2'>
+                                        <li>Men</li>
+                                        <li>Women</li>
+                                        <li>Accessories</li>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className='BestRow2'>
+                                <div className='ProductGroup'>
+                                    <div className='GroupContent'>
+                                        <div className='GroupTab'>
+                                            <div className='GroupTab1'>
+                                                <div className='GroupTabProduct'></div>
+                                                <div className='ItemCard'>
+                                                    <div className='ItemCard1'>
+                                                        <img src="#" alt="" />
+                                                    </div>
+                                                    <div className='ItemCard2'>
+                                                        <h5>Graphic Design</h5>
+                                                        <p>English Department</p>
+                                                        {/* p tagi link olarak ürün kategorisi sayfasına gidecek  */}
+                                                        <div className='Pricess'>
+                                                            <p>$16.48</p>
+                                                            <p>$6.48</p>
+                                                        </div>
+                                                    </div>
+                                                </div>
+
+                                                <div className='GroupTabProduct'>
+                                                    <div className='ItemCard'>
+                                                        <div className='ItemCard1'>
+                                                            <img src="#" alt="" />
+                                                        </div>
+                                                        <div className='ItemCard2'>
+                                                            <h5>Graphic Design</h5>
+                                                            <p>English Department</p>
+                                                            {/* p tagi link olarak ürün kategorisi sayfasına gidecek  */}
+                                                            <div className='Pricess'>
+                                                                <p>$16.48</p>
+                                                                <p>$6.48</p>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <div className='GroupTabProduct'>
+                                                    <div className='ItemCard'>
+                                                        <div className='ItemCard1'>
+                                                            <img src="#" alt="" />
+                                                        </div>
+                                                        <div className='ItemCard2'>
+                                                            <h5>Graphic Design</h5>
+                                                            <p>English Department</p>
+                                                            {/* p tagi link olarak ürün kategorisi sayfasına gidecek  */}
+                                                            <div className='Pricess'>
+                                                                <p>$16.48</p>
+                                                                <p>$6.48</p>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div className='GroupTab2'>
+                                                <div className='GroupTabProduct'>
+                                                    <div className='ItemCard'>
+                                                        <div className='ItemCard1'>
+                                                            <img src="#" alt="" />
+                                                        </div>
+                                                        <div className='ItemCard2'>
+                                                            <h5>Graphic Design</h5>
+                                                            <p>English Department</p>
+                                                            {/* p tagi link olarak ürün kategorisi sayfasına gidecek  */}
+                                                            <div className='Pricess'>
+                                                                <p>$16.48</p>
+                                                                <p>$6.48</p>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <div className='GroupTabProduct'>
+                                                    <div className='ItemCard'>
+                                                        <div className='ItemCard1'>
+                                                            <img src="#" alt="" />
+                                                        </div>
+                                                        <div className='ItemCard2'>
+                                                            <h5>Graphic Design</h5>
+                                                            <p>English Department</p>
+                                                            {/* p tagi link olarak ürün kategorisi sayfasına gidecek  */}
+                                                            <div className='Pricess'>
+                                                                <p>$16.48</p>
+                                                                <p>$6.48</p>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <div className='GroupTabProduct'>
+                                                    <div className='ItemCard'>
+                                                        <div className='ItemCard1'>
+                                                            <img src="#" alt="" />
+                                                        </div>
+                                                        <div className='ItemCard2'>
+                                                            <h5>Graphic Design</h5>
+                                                            <p>English Department</p>
+                                                            {/* p tagi link olarak ürün kategorisi sayfasına gidecek  */}
+                                                            <div className='Pricess'>
+                                                                <p>$16.48</p>
+                                                                <p>$6.48</p>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
