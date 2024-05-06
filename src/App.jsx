7254ff379a185a1f84e7a69d16a16f57b6d22358
mertbatut <mertbatut@gmail.com>
2024-05-06 16:18:18 +0300

    Product-Card-25 Güncelleme1

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { BrowserRouter } from 'react-router-dom/cjs/react-router-dom.min'
 import Carousel from './components/Carousels'
 import ShopCard from './components/ShopCard'
 import Footer from './layout/Footer'
+import ProductContent from './layout/PageContent'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -15,6 +16,7 @@ function App() {
         <Header/>
         <Carousel/>
         <ShopCard/>
+        <ProductContent/>
         <Footer/>
       </BrowserRouter>
     </>
--- src/layout/PageContent.jsx
+++ src/layout/PageContent.jsx
@@ -1,49 +1,51 @@
 import React, { Component } from 'react'
 
-export default class
+export default class ProductContent 
     extends Component {
     render() {
         return (
-            <div>
-                <div className='BestSeller'>
-                    <div className='FurnitureCard'>
-                        <div className='ItemFurniture'>
-                            <h6>FURNITURE</h6>
-                            <h6>5 Items</h6>
+            <div ProductContainer>
+                <div className='BestSeller flex justify-center px-6 py-6 gap-12'>
+                    <div className='FurnitureCard flex '>
+                        <div className='ItemFurniture absolute gap-1.5 flex flex-col m-8'>
+                            <h6 className='font-bold text-sm text-[#252B42] leading-6'>FURNITURE</h6>
+                            <h6 className='font-bold text-sm text-[#737373] leading-6'>5 Items</h6>
                         </div>
                         <div className='FurBackground'>
-                            <img src="#" alt="" />
+                            <img src="../images/furniture.jpg" alt="" />
                         </div>
                     </div>
                     <div className='BestProduct'>
-                        <div className='BestMain'>
-                            <div className='BestRow1'>
-                                <div className='BestContent'>
-                                    <h3>BESTSELLER PRODUCTS</h3>
+                        <div className='BestMain '>
+                            <div className='BestRow1 flex gap-2.5 items-center '>
+                                <div className='BestContent font-bold text-base leading-6 text-[#252B42]'>
+                                    <h3 className='flex gap-2.5 '>BESTSELLER PRODUCTS</h3>
                                 </div>
-                                <div className='BestNav'>
-                                    <div className='BestNav1'>
-                                        <li>Sola ok</li>
-                                        <li>Sağa ok</li>
+                                <div className='BestNav flex list-none gap-2.5 items-center '>
+                                    <div className='BestNav1 flex gap-2.5 '>
+                                        <li className='text-[#23A6F0] font-bold text-sm'><button className='px-2.5 py-5' > Men</button></li>
+                                        <li className='text-[#737373] font-bold text-sm'><button className='px-2.5 py-5'> Women</button></li>
+                                        <li className='text-[#737373] font-bold text-sm'><button className='px-2.5 py-5'>Accessories</button></li>
+
                                     </div>
-                                    <div className='BestNav2'>
-                                        <li>Men</li>
-                                        <li>Women</li>
-                                        <li>Accessories</li>
+                                    <div className='BestNav2 flex gap-2.5'>
+                                    <li className='text-[#737373] font-bold text-sm'><button className='px-2.5 py-5'> Sola ok</button></li>
+                                        <li className='text-[#737373] font-bold text-sm'><button className='px-2.5 py-5'>Sağa ok</button></li>
                                     </div>
                                 </div>
                             </div>
+                            <hr className='text-[#ECECEC] h-0.5 w-[638px]' />
                             <div className='BestRow2'>
                                 <div className='ProductGroup'>
-                                    <div className='GroupContent'>
-                                        <div className='GroupTab'>
-                                            <div className='GroupTab1'>
-                                                <div className='GroupTabProduct'></div>
-                                                <div className='ItemCard'>
-                                                    <div className='ItemCard1'>
-                                                        <img src="#" alt="" />
+                                    <div className='GroupContent gap-5 py-2'>
+                                        <div className='GroupTab p-6 gap-4'>
+                                            <div className='GroupTab1 pt-4 flex gap-12'>
+                                                <div className='GroupTabProduct'>
+                                                <div className='ItemCard flex flex-col items-center'>
+                                                    <div className='ItemCard1 h-[162px] w-[183px]'>
+                                                        <img src="../images/product1.png" alt="" />
                                                     </div>
-                                                    <div className='ItemCard2'>
+                                                    <div className='ItemCard2 flex flex-col items-center'>
                                                         <h5>Graphic Design</h5>
                                                         <p>English Department</p>
                                                         {/* p tagi link olarak ürün kategorisi sayfasına gidecek  */}
@@ -53,13 +55,15 @@ export default class
                                                         </div>
                                                     </div>
                                                 </div>
+                                                </div>
+                                                
 
                                                 <div className='GroupTabProduct'>
-                                                    <div className='ItemCard'>
-                                                        <div className='ItemCard1'>
-                                                            <img src="#" alt="" />
+                                                    <div className='ItemCard ItemCard flex flex-col items-center'>
+                                                        <div className='ItemCard1 h-[162px] w-[183px]'>
+                                                            <img src="../images/product2.jpg" alt="" />
                                                         </div>
-                                                        <div className='ItemCard2'>
+                                                        <div className='ItemCard2 flex flex-col items-center'>
                                                             <h5>Graphic Design</h5>
                                                             <p>English Department</p>
                                                             {/* p tagi link olarak ürün kategorisi sayfasına gidecek  */}
@@ -71,11 +75,11 @@ export default class
                                                     </div>
                                                 </div>
                                                 <div className='GroupTabProduct'>
-                                                    <div className='ItemCard'>
-                                                        <div className='ItemCard1'>
-                                                            <img src="#" alt="" />
+                                                    <div className='ItemCard ItemCard flex flex-col items-center'>
+                                                        <div className='ItemCard1 h-[162px] w-[183px]'>
+                                                            <img src="../images/product3.jpg" alt="" />
                                                         </div>
-                                                        <div className='ItemCard2'>
+                                                        <div className='ItemCard2 flex flex-col items-center'>
                                                             <h5>Graphic Design</h5>
                                                             <p>English Department</p>
                                                             {/* p tagi link olarak ürün kategorisi sayfasına gidecek  */}
@@ -87,13 +91,31 @@ export default class
                                                     </div>
                                                 </div>
                                             </div>
-                                            <div className='GroupTab2'>
+                                            <div className='GroupTab1 pt-12 flex gap-12'>
                                                 <div className='GroupTabProduct'>
-                                                    <div className='ItemCard'>
-                                                        <div className='ItemCard1'>
-                                                            <img src="#" alt="" />
+                                                <div className='ItemCard ItemCard flex flex-col items-center'>
+                                                    <div className='ItemCard1 h-[162px] w-[183px]'>
+                                                        <img src="../images/product1.png" alt="" />
+                                                    </div>
+                                                    <div className='ItemCard2 flex flex-col items-center'>
+                                                        <h5>Graphic Design</h5>
+                                                        <p>English Department</p>
+                                                        {/* p tagi link olarak ürün kategorisi sayfasına gidecek  */}
+                                                        <div className='Pricess'>
+                                                            <p>$16.48</p>
+                                                            <p>$6.48</p>
                                                         </div>
-                                                        <div className='ItemCard2'>
+                                                    </div>
+                                                </div>
+                                                </div>
+                                                
+
+                                                <div className='GroupTabProduct'>
+                                                    <div className='ItemCard ItemCard flex flex-col items-center'>
+                                                        <div className='ItemCard1 h-[162px] w-[183px]'>
+                                                            <img src="../images/product2.jpg" alt="" />
+                                                        </div>
+                                                        <div className='ItemCard2 flex flex-col items-center'>
                                                             <h5>Graphic Design</h5>
                                                             <p>English Department</p>
                                                             {/* p tagi link olarak ürün kategorisi sayfasına gidecek  */}
@@ -105,27 +127,11 @@ export default class
                                                     </div>
                                                 </div>
                                                 <div className='GroupTabProduct'>
-                                                    <div className='ItemCard'>
-                                                        <div className='ItemCard1'>
-                                                            <img src="#" alt="" />
+                                                    <div className='ItemCard ItemCard flex flex-col items-center'>
+                                                        <div className='ItemCard1 h-[162px] w-[183px]'>
+                                                            <img src="../images/product3.jpg" alt="" />
                                                         </div>
-                                                        <div className='ItemCard2'>
-                                                            <h5>Graphic Design</h5>
-                                                            <p>English Department</p>
-                                                            {/* p tagi link olarak ürün kategorisi sayfasına gidecek  */}
-                                                            <div className='Pricess'>
-                                                                <p>$16.48</p>
-                                                                <p>$6.48</p>
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                                <div className='GroupTabProduct'>
-                                                    <div className='ItemCard'>
-                                                        <div className='ItemCard1'>
-                                                            <img src="#" alt="" />
-                                                        </div>
-                                                        <div className='ItemCard2'>
+                                                        <div className='ItemCard2 flex flex-col items-center'>
                                                             <h5>Graphic Design</h5>
                                                             <p>English Department</p>
                                                             {/* p tagi link olarak ürün kategorisi sayfasına gidecek  */}
