2a095db3b5c0c6c9b900bc69fcd15c2a936522fb
mertbatut <mertbatut@gmail.com>
2024-05-11 14:59:32 +0300

    RouterDom Düzen1

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,8 @@
 import { useState } from 'react'
 import './App.css'
-import Header from './layout/Header'
 import { BrowserRouter } from 'react-router-dom/cjs/react-router-dom.min'
-import Carousel from './components/Carousels'
-import ShopCard from './components/ShopCard'
-import Footer from './layout/Footer'
-import ProductContent from './layout/PageContent'
-import ProductCard4 from './components/ProductCard4'
-import ProductCard25 from './components/Productcard25'
-import ProductCard40 from './components/ProductCard40'
-import ShopCard2 from './components/ShopCard2'
-import Blog from './components/blog'
-import Drop from './components/dropdown'
+
+import HomePage from './pages/HomePage'
 
 
 function App() {
@@ -20,17 +11,7 @@ function App() {
   return (
     <>
       <BrowserRouter>
-        {/* <Header/>
-        <Carousel/>
-        <ShopCard/>
-        <ProductContent/>
-        <ProductCard4/>
-        <ProductCard25/>
-        <ProductCard40/>
-        <ShopCard2/>
-        <Blog/>
-        <Footer/>  */}
-      <Drop></Drop>
+      <HomePage/>
       </BrowserRouter>
     </>
   )
