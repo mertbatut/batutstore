c0ad445a2d9832eea0cb81586c0d9526abcf208e
mertbatut <mertbatut@gmail.com>
2024-05-13 15:48:51 +0300

    Contact Tamamlandı

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -8,7 +8,7 @@ export default class Contact extends Component {
         <Header/>
        <div className='ContactTotal '>
         <div className='ContactContainer '>
-            <div className='ContactRow absolute flex items-center gap-24 pl-80 pt-28'>
+            <div className='ContactRow absolute flex items-center gap-24 pl-80 pt-28 '>
                 <div className='ContactText1 flex flex-col gap-9 w-[367px] h-[234px]'>
                     <h2 className='font-bold text-[40px] text-[#FFFFFF] '>CONTACT US</h2>
                     <p className='text-sm font-normal text-[#FFFFFF]'>Problems trying to resolve the conflict between the two major realms of Classical physics: Newtonian mechanics </p>
@@ -55,8 +55,8 @@ export default class Contact extends Component {
                 </div>
             </div>
         </div>
-        <div className='ContactImage'>
-            <img className=' w-full h-[757px] '  src="https://s3-alpha-sig.figma.com/img/96c8/6912/d491d421800e62998b9af7c838cc25d1?Expires=1716768000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=ME~~D9gkMEvSPsSJVuH6KA0yA13zvRIbbxyYbKoZrLpoSY9OjuujJf5cCvlKL4KM4ukI-zHIC6~~45OV5d4KjiPI8soTaAHD9fMT7UjncHaCkVYK68cmPWrdNCSXlYjuZESEkFaKTKMMiP3Ky9FN7r2n-41pEmamlpt-q4z29M1shQrSkYTGTFTeAT8kk1Fu48cB0QgV2Ze9L5m3k07oRPFvy99XjT3arKAvbN4VISMUm7SZ9Tuc6JOKeBaH0eED8lkPrH~uGVaJ~zl9uPEwuuNbtvPtWQP0ngOVp4mJFzqRX2xUgJ7hWBxNwTofpgu20QxRs0JWABruTRndC5c72A__" alt="" />
+        <div className='ContactImage '>
+            <img className=' w-full h-[757px]  "'  src="https://s3-alpha-sig.figma.com/img/96c8/6912/d491d421800e62998b9af7c838cc25d1?Expires=1716768000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=ME~~D9gkMEvSPsSJVuH6KA0yA13zvRIbbxyYbKoZrLpoSY9OjuujJf5cCvlKL4KM4ukI-zHIC6~~45OV5d4KjiPI8soTaAHD9fMT7UjncHaCkVYK68cmPWrdNCSXlYjuZESEkFaKTKMMiP3Ky9FN7r2n-41pEmamlpt-q4z29M1shQrSkYTGTFTeAT8kk1Fu48cB0QgV2Ze9L5m3k07oRPFvy99XjT3arKAvbN4VISMUm7SZ9Tuc6JOKeBaH0eED8lkPrH~uGVaJ~zl9uPEwuuNbtvPtWQP0ngOVp4mJFzqRX2xUgJ7hWBxNwTofpgu20QxRs0JWABruTRndC5c72A__" alt="" />
         </div>
         </div>
       </div>
