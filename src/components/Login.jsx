d1c9554253ad394b9e840d4351201ab508dd948f
mertbatut <mertbatut@gmail.com>
2024-05-15 17:19:22 +0300

    Login Tasarım

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,14 +1,20 @@
 import React from 'react'
 import Header from './Header';
+import Footer from './Footer';
 
 const LoginPage = () => {
   return (
    
-   <div >
-    <Header/>
-        <div className='LoginStyle'>
-        <div className='LoginTotal shadow-lg  w-[580px]  px-16 py-16 bg-[#26442b] rounded-3xl'>
-            <h1 className='flex pb-8 font-bold text-5xl text-[#FFFFFF]'>Login</h1>
+            
+    <div >
+    <div><Header/></div>
+<div className='LoginBrand flex justify-between pb-20'>   
+        <div className='LoginImg  '>
+            <img className='h-screen w-[1100px] rounded-lg ' src="https://s3-alpha-sig.figma.com/img/b178/ccf1/36301d841438b7cd53ac541b247fb3fa?Expires=1716768000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=AXY5OZ3SNhv-43G6OPaAV~EiWArHWj5TP2NqCY5zo~jOfN2zxRQX23LxE6UImKS3w3u4o7ZQA5jAgZs3TJ15j6yMrsifmfc66-kLo5yonf7xKFEvk-GtvZo~D0fpYgVfAXjP4qP22kXrj8AphEYl8Rv5LdD7c2fgtekw~ZVfqC-LrN7OMwhUOa3TRitnPN~fgBGKD9Yi6ltr4BD-T7~tka0wlYS1WXPIlBHPyRraHPsMA1N8C0l7-oZ8EJEDcVdJHINNUVtcZScOm6d0wyPPdlJSix55YlEwSQvof6Yl1nOFt5iNES2Z6PFyHLzY7IRXAcVzRdtHr4y12JBzbCfNEw__" alt="" />
+        </div>
+        <div className='LoginStyle pt-40 px-28 '>
+        <div className='LoginTotal shadow-lg  w-[580px]  px-16 py-16 bg-[#c6d1c8] rounded-3xl drop-shadow-2xl'>
+            <h1 className=' pb-8 font-bold text-5xl text-[#FFFFFF]'>Login</h1>
             <div className='LoginRow1 flex flex-col gap-3 pb-3'>
                 <h1 className='text-[#FFFFFF]'>Mail Adresinizi Giriniz</h1>
                 <input className='w-[400px] h-[40px] rounded-md border-2' type="mail" placeholder='E-mail'/>
@@ -22,6 +28,8 @@ const LoginPage = () => {
             <div className='LoginRow3'></div>
         </div>
         </div>
+        </div>  
+        <Footer/>
         </div>
   )
 }
